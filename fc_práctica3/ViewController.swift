2ff65971ca3406b13da4fb6c98424f2b07040895
Carlos Villa <carlosvilla@Carloss-Mac.local>
2017-10-26 12:18:43 +0200

    Initial Commit

---
+++ fc_práctica3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fc_práctica3
+//
+//  Created by Carlos Villa on 26/10/2017.
+//  Copyright © 2017 Carlos Fernando. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
